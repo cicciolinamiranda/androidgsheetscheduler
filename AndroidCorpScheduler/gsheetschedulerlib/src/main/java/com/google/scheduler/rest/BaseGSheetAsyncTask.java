87afc36a264a91caba00db2fa31ff7c7e711c09d
Cicciolina Miranda <cicciolina@cicciolina-macbookpro.roam.corp.google.com>
2018-06-06 18:49:12 +0800

    make allow back up false. so when app is deleted. credentials will be deleted as well

---
+++ AndroidCorpScheduler/gsheetschedulerlib/src/main/java/com/google/scheduler/rest/BaseGSheetAsyncTask.java
@@ -0,0 +1,73 @@
+package com.google.scheduler.rest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.scheduler.R;
+
+import java.io.IOException;
+
+/**
+ * Created by cicciolina on 6/6/18.
+ */
+
+public abstract class BaseGSheetAsyncTask extends AsyncTask<Object, Void, Object> {
+
+    protected Context context;
+    protected String spreadsheetId;
+    protected String tabSheetName;
+    protected BaseGSheetListener listener;
+    protected com.google.api.services.sheets.v4.Sheets mService = null;
+    protected HttpTransport transport;
+    protected JsonFactory jsonFactory;
+
+    public BaseGSheetAsyncTask(GoogleAccountCredential credential,
+                               Context context,
+                               BaseGSheetListener listener,
+                               String spreadsheetId,
+                               String tabSheetName) {
+        this.context = context;
+        this.spreadsheetId = spreadsheetId;
+        this.tabSheetName = tabSheetName;
+        this.listener = listener;
+        transport = AndroidHttp.newCompatibleTransport();
+        jsonFactory = JacksonFactory.getDefaultInstance();
+        mService = new com.google.api.services.sheets.v4.Sheets.Builder(
+                transport, jsonFactory, credential)
+                .setApplicationName(context.getString(R.string.app_name))
+                .build();
+    }
+
+    @Override
+    protected Object doInBackground(Object... params) {
+        try {
+            return getDataFromApi(params);
+        } catch (UserRecoverableAuthIOException e) {
+            listener.requestForAuthorization(e.getIntent());
+            e.printStackTrace();
+            cancel(true);
+            return null;
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            cancel(true);
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            cancel(true);
+            return null;
+        }
+    }
+
+    public abstract Object getDataFromApi(Object... params) throws IOException;
+
+    public interface BaseGSheetListener {
+        void requestForAuthorization(Intent intent);
+    }
+}
---
+++ AndroidCorpScheduler/gsheetschedulerlib/src/main/java/com/google/scheduler/rest/RestGetLobInGSheet.java
@@ -0,0 +1,68 @@
+package com.google.scheduler.rest;
+
+import android.content.Context;
+
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by cicciolina on 6/6/18.
+ */
+
+public class RestGetLobInGSheet extends BaseGSheetAsyncTask {
+
+    private Listener listener;
+    private final static String RANGE = "B2:B";
+
+    public RestGetLobInGSheet(GoogleAccountCredential credential,
+                                           Context context, Listener listener, String spreadsheetId,
+                                           String tabSheetName) {
+        super(credential, context, listener, spreadsheetId, tabSheetName);
+        this.listener = listener;
+    }
+
+    @Override
+    public List<List<Object>>  getDataFromApi(Object... params) throws IOException {
+        List<List<Object>> results = new ArrayList<>();
+
+        String commentRange = tabSheetName+"!"+RANGE;
+
+        ValueRange sheetRange = mService.spreadsheets().values()
+                .get(spreadsheetId, commentRange)
+                .execute();
+
+        if(sheetRange != null && sheetRange.getValues() != null &&
+                !sheetRange.getValues().isEmpty()) {
+            results = sheetRange.getValues();
+        }
+        return results;
+    }
+
+
+    @Override
+    protected void onPostExecute(Object output) {
+        listener.result(translateToLobStr((List<List<Object>>)output));
+    }
+
+    private ArrayList<String> translateToLobStr(List<List<Object>> values){
+        ArrayList<String>  lobList = new ArrayList<>();
+
+        for(List<Object> lobArr: values){
+
+            lobList.add((String)lobArr.get(0));
+
+        }
+
+        return lobList;
+    }
+
+
+
+    public interface Listener extends BaseGSheetListener {
+        void result(ArrayList<String> lobList);
+    }
+}
